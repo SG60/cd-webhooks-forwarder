36d5f14470a5608515fc087316c59c4a6b950f79
Sam Greening <2552620+SG60@users.noreply.github.com>
2023-12-23 23:51:32 +0000

    nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Flake for cd-webhooks-forwarder";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = with pkgs; mkShell {
+            packages = [ protobuf ];
+          };
+        };
+      });
+}
