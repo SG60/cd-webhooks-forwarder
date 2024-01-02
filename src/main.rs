95f5e77740f2f4a2166b0880d153c144cb85363b
Sam Greening <2552620+SG60@users.noreply.github.com>
2024-01-02 21:52:12 +0000

    use local copy of otel tracing utils

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,8 @@ async fn main() -> Result<()> {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
+    opentelemetry_tracing_utils::shutdown_tracer_provider();
+
     Ok(())
 }
 fn app(state: AppState) -> Router {
